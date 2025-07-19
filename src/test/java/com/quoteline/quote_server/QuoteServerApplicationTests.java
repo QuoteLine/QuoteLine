d885e4bba5028e4d54704571af8b2a914510374a
chanung <1stevering@naver.com>
2025-07-19 17:15:18 +0900

    초기 세팅

---
+++ src/test/java/com/quoteline/quote_server/QuoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quoteline.quote_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
