816c0763cace49ef39cbe5250a9f62aa96736d80
chanung <1stevering@naver.com>
2025-07-26 16:59:21 +0900

    feat: 이메일 중복 에러 처리

--- src/main/java/com/quoteline/quote_server/subscription/service/SubscriptionService.java
+++ src/main/java/com/quoteline/quote_server/subscription/service/SubscriptionService.java
@@ -1,5 +1,7 @@
 package com.quoteline.quote_server.subscription.service;
 
+import com.quoteline.quote_server.exception.ErrorCode;
+import com.quoteline.quote_server.exception.QuoteLineException;
 import com.quoteline.quote_server.subscription.domain.Subscription;
 import com.quoteline.quote_server.subscription.dto.SubscriptionRequest;
 import com.quoteline.quote_server.subscription.repository.SubscriptionRepository;
@@ -13,6 +15,10 @@ public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
 
     public Subscription createSubscription(SubscriptionRequest request) {
+        if(subscriptionRepository.existsByEmail(request.getEmail())) {
+            throw new QuoteLineException(ErrorCode.REPEATED_EMAIL, "");
+        }
+
         Subscription subscription = Subscription.builder()
                 .age(request.getAge())
                 .gender(request.getGender())
