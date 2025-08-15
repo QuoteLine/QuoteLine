c7145306895b5d35956fef0e0300950ce23d7fca
1stevering <1stevering@naver.com>
2025-08-15 12:10:54 +0900

    feat: Grafana, Prometheus 모니터링 시스템 구축

--- src/main/java/com/quoteline/quote_server/notification/service/NotificationConsumer.java
+++ src/main/java/com/quoteline/quote_server/notification/service/NotificationConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 @Slf4j
 @RequiredArgsConstructor
 public class NotificationConsumer {
--- src/main/java/com/quoteline/quote_server/quote/service/QuoteService.java
+++ src/main/java/com/quoteline/quote_server/quote/service/QuoteService.java
@@ -1,6 +1,7 @@
 package com.quoteline.quote_server.quote.service;
 
-import lombok.AllArgsConstructor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,9 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Service
-@AllArgsConstructor
 public class QuoteService {
     private final RedisTemplate<String, String> redisTemplate;
+    private final Counter quoteRequestCounter;
     private static final String CACHE_KEY = "DAILY_QUOTE";
     private static final List<String> DUMMY_QUOTES = List.of(
             "그냥 해. 변명이 왜 이렇게 많아",
@@ -19,7 +20,15 @@ public class QuoteService {
     );
     private final Random random = new Random();
 
+    public QuoteService(RedisTemplate<String, String> redisTemplate, MeterRegistry meterRegistry) {
+        this.redisTemplate = redisTemplate;
+        this.quoteRequestCounter = Counter.builder("quote.requests")
+                .description("Number of quote requests")
+                .register(meterRegistry);
+    }
+
     public String getQuote() {
+        quoteRequestCounter.increment();
         String cached = redisTemplate.opsForValue().get(CACHE_KEY);
         if(cached != null) {
             return cached;
