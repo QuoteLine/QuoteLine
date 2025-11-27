8318b80c5f523eec46e1f8f72bd707f056acd37e
1stevering <1stevering@naver.com>
2025-11-27 13:27:25 +0900

    refactor: boardRequest 유효성 검사 추가

--- src/main/java/com/quoteline/quote_server/board/dto/BoardRequest.java
+++ src/main/java/com/quoteline/quote_server/board/dto/BoardRequest.java
@@ -1,5 +1,7 @@
 package com.quoteline.quote_server.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardRequest {
+    @NotBlank(message = "제목은 필수입니다")
+    @Size(max = 255, message = "제목은 255자를 초과할 수 없습니다")
     private String title;
+
+    @NotBlank(message = "내용은 필수입니다")
     private String content;
     private String author;
 }
