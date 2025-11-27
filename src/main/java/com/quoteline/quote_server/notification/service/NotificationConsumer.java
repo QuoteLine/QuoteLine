95acab35600f7bfa852c1614bfe8e183db97fbb0
1stevering <1stevering@naver.com>
2025-11-27 16:32:39 +0900

    fix: 빌드 에러 수정

--- src/main/java/com/quoteline/quote_server/notification/service/NotificationConsumer.java
+++ src/main/java/com/quoteline/quote_server/notification/service/NotificationConsumer.java
@@ -3,16 +3,22 @@ package com.quoteline.quote_server.notification.service;
 import com.quoteline.quote_server.notification.domain.EmailPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NotificationConsumer {
-    private final JavaMailSender mailSender;
+    private final Optional<JavaMailSender> mailSender;
+
+    @Value("${app.mail.enabled:false}")
+    private boolean mailEnabled;
 
     @KafkaListener(topics = "#{'${app.kafka.topic.email:email-topic}'}", groupId = "quote-mail")
     public void consume(EmailPayload payload) {
@@ -21,7 +27,7 @@ public class NotificationConsumer {
         msg.setTo(payload.getEmail());
         msg.setSubject("오늘의 명언");
         msg.setText(payload.getQuote());
-        mailSender.send(msg);
+        mailSender.get().send(msg);
         log.info("Send email to {} with quote: {}", payload.getEmail(), payload.getQuote());
     }
 }
